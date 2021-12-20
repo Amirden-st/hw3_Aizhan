14696c81c58615b43d62a17c173414e88c5cef08
Amirden-st <82529791+Amirden-st@users.noreply.github.com>
2021-12-21 00:34:06 +0600

    Update aizhan.js

--- aizhan.js
+++ aizhan.js
@@ -1,7 +1,8 @@
+//          можно опустить скобки
 var string = ("abbccdddd")
 var num = []
 
-for (var i = 0; i < string.length; i++)
+for (var i = 0; i < string.length; i++) 
     if (num[string[i]]) {
         num [string[i]]++;
     } else {
@@ -9,12 +10,27 @@ for (var i = 0; i < string.length; i++)
     }
 console.log(num);
 
+// не совсем то, но близко)
+// нужно было написать функцию:
+function countChar(string, char) {
+    // заводим переменную для счёта буквы char в строке (string)
+    var count = 0
+    for (var i = 0; i < string.length; i++) {
+        // обходим строку по одному символу и проверяем на совпадение с char
+        if (string[i] === char) count++ 
+    }
+    return count
+}
+
+countChar('dwdadwdeawdrfffff', 'f') // 5 букв f внутри переданной строки 
+
+
 function getMin(arr) {
     return Math.min(...arr)
 }
 
 console.log(getMin([1, 2, 3, 4, 5]));
-
+// Зачем писать её второй раз?)
 function getMin(arr) {
     return Math.min(...arr)
 }
@@ -73,6 +89,7 @@ function getProduct(productName){
     function callback(product){
         if(product.productName===productName) return  true
     }
+    // отлично!
     var foundProduct=products.find(callback)
     return foundProduct
 }
